2a249f8f3e868a3f83273cb40135930ae0976ae6
Sivaramalingam R <90626897+siva1196@users.noreply.github.com>
2022-01-26 16:48:24 -0600

    Ping Monitoring
    Ping Monitoring

---
+++ SertiPing.ps1
@@ -0,0 +1,51 @@
+﻿Do{
+    $Servers = @()
+    $IP = @()
+    $Location = @()
+    $PManager = @()
+    $ServerDownCount = 0
+
+    $ServerList = Import-csv 'C:\Users\coramals\Desktop\Serti.csv' |`
+
+    ForEach-Object{
+
+        $Servers+=$_.Server
+        $IP+=$_."IP Address"
+        $Location+=$_.Location
+        $PManager+=$_."Plant manager"
+
+    }
+
+    Write-Host(Get-Date)
+
+    foreach ($Server in $Servers){
+
+        $Ping = Test-Connection -ComputerName $Server -Count 1 -Quiet
+
+            if($Ping){ 
+
+                Write-Host "$Server is Online" -BackgroundColor Green
+
+            }
+
+            else {
+        
+                Write-Host "$Server is Offline" -BackgroundColor Red
+                $ServerDownCount +=1
+            }
+
+        }
+        Write-Host "Server down Count = $ServerDownCount"
+
+        Write-Host -NoNewline "Pausing "
+
+        for($s=0; $s -le 5; $s++){
+
+            Write-Host -NoNewline "."
+            Start-Sleep 3
+        }
+
+        Write-Host ""
+}
+
+while($true)
